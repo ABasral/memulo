91bdbf068c73ecc48ad3b9a3e83e2f2378bea456
Ayush <Ayush@macbook.local>
2022-10-16 03:50:19 +0530

    first commit

--- app/src/main/java/com/example/memulo/teacherform.java
+++ app/src/main/java/com/example/memulo/teacherform.java
@@ -2,16 +2,63 @@ package com.example.memulo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class teacherform extends AppCompatActivity {
+
+    private Spinner spinner;
+    private static final String[] paths = {"Sub1", "Sub2", "sub3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacherform);
 
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(teacherform.this,
+                android.R.layout.simple_spinner_item, paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        Button register = (Button) findViewById(R.id.rbutton);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(teacherform.this, studenthome.class);
+                // myIntent.putExtra("key", value);
+                teacherform.this.startActivity(myIntent);
+            }
+        });
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+
+        }
+    }
 
 
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 }
